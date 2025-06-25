d625332db87053d0c06d6db01c5d9540dcb36ee7
regenouis <70100775+regenouis@users.noreply.github.com>
2025-06-25 16:41:16 -0500

    Create mpb_scraper.py

---
+++ scrapers/mpb_scraper.py
@@ -0,0 +1,7 @@
+def check_mpb(product_name, mpn):
+    return {
+        "retailer": "MPB",
+        "status": "Placeholder (module not yet implemented)",
+        "price": None,
+        "url": None
+    }
