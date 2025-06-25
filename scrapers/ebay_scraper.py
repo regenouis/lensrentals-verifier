93261d94f8cb2977fb6128e9a19521a61e2c67a9
regenouis <70100775+regenouis@users.noreply.github.com>
2025-06-25 16:39:16 -0500

    Update ebay_scraper.py

--- scrapers/ebay_scraper.py
+++ scrapers/ebay_scraper.py
@@ -1,1 +1,8 @@
+def check_ebay(product_name, mpn):
+    return {
+        "retailer": "eBay",
+        "status": "Placeholder (module not yet implemented)",
+        "price": None,
+        "url": None
+    }
 
